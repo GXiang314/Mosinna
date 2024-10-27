c2556ef1c47fd6635bf20b34f9ac2717e0f02598
GXiang314 <akz232032@gmail.com>
2024-10-28 01:51:13 +0800

    refactor: extract enum.

--- backend/src/service/video.service.js
+++ backend/src/service/video.service.js
@@ -1,23 +1,32 @@
 import videoRepository from '../repository/video.repository'
 import serviceRepository from '../repository/service.repository'
 import axios from 'axios'
+import { EXTERNAL_SERVICE_NAME } from '../enum/external-service-name.enum'
 
 export class VideoService {
+    /**
+     *
+     * @param {string} dataUrl
+     * @returns {Promise<string>}
+     */
     async saveVideo(dataUrl) {
         const video = await videoRepository.saveVideo(dataUrl)
         return video.id
     }
 
+    /**
+     *
+     * @param {string} url
+     * @returns {Promise<string>}
+     */
     async getVideoData(url) {
         const url2VideoService = (
             await serviceRepository.getAllAvaliableServices()
         ).find((x) => {
-            return x.name === 'URL轉換影片'
+            return x.name === EXTERNAL_SERVICE_NAME.URL2BASE64
         })
         if (!url2VideoService)
-            throw new Error(
-                '無可用的 ＵＲＬ 解析服務，註冊：https://ai3.gxiangsoft.com/api/url2base64',
-            )
+            throw new Error('無可用的 URL 解析服務，請聯絡系統管理員')
         return (await axios.get(`${url2VideoService.host}?url=${url}`)).data
             ?.result
     }
