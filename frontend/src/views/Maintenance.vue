faf12a1d727562d4f11079f621832595b6de1422
GXiang314 <akz232032@gmail.com>
2024-11-13 17:21:58 +0800

    feat: add Maintain mode feature toggle.

---
+++ frontend/src/views/Maintenance.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="h-[90vh]">
+    <div
+      class="h-[300px] md:h-[300px] sm:h-[200px] bg-contain bg-no-repeat bg-center flex justify-center items-center"
+      style="background-image: url('/assets/image/vision.png')"
+    ></div>
+
+    <div class="bottom-0 w-full flex justify-center">
+      <div
+        class="w-full bg-[hsla(282,18%,39%,0.7)] border border-[#f1ecff] border-b-0 rounded-t-[20px] p-8 shadow-[0_-2px_10px_rgba(0,0,0,0.1)]"
+      >
+        <div class="flex flex-col items-center justify-center">
+          <h1 class="text-4xl font-bold text-[#f1ecff] mb-4">網站維護中</h1>
+          <p class="text-xl text-[#f1ecff] text-center">
+            我們正在進行系統維護與升級，<br />
+            請稍後再回來訪問。
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
