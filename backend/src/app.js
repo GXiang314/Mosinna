2ece78854edab949ba3b87ddf3207777e6f7897b
GXiang <gxiang@gxiangsoft.com>
2025-04-30 15:35:59 +0800

    fix(backend): allow origin all

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,11 @@ app.use(
         crossOriginResourcePolicy: false,
     }),
 )
-app.use(cors())
+app.use(
+    cors({
+        origin: '*',
+    }),
+)
 app.use(express.json({ limit: '50mb' }))
 app.use(
     '/resources',
